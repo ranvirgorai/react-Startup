2341fb45c9a3b3163d49fd82a22cfb0b3a8684df
Ranvir Gorai <ranvir@wslab.in>
2017-03-01 15:02:56 +0530

    Side bar included

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import MainNavbar from './components/navbar';
 import SideBar from './components/sidebar';
+import { Col, Row}  from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <MainNavbar/>
-      <SideBar/>
+      <Row>
+          <Col xs={1}>
+            <SideBar/>
+          </Col>
+          <Col xs={11}></Col>
+          </Row>
       </div>
     );
   }
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,34 +1,48 @@
-import React from 'react';
-import styles from './style.css';
-import {Nav, NavItem, Navbar, NavDropdown, MenuItem, Glyphicon} from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Glyphicon} from 'react-bootstrap';
+import './style.css';
+class SideBar extends Component {
+	render() {
+		return (
+			<div>
+				<div className="row" >
+					<div className="col-xs-1">
+						<ul id="sidebar" className="nav nav-stacked affix">
+							<li>
+                <a href="#">
+                <Glyphicon glyph="star" /><p></p>
+								 Dashboard</a>
+							</li>
+							<li><a href="#">
+                <Glyphicon glyph="star" /><p></p>
+								 Home </a>
+							</li>
+							<li>
+                <a href="#">
+                <Glyphicon glyph="star" /><p></p>
+								 MIS</a>
+							</li>
+							<li>
+                <a href="#">
+                <Glyphicon glyph="star" /><p></p>
+								 Item 1</a>
+							</li>
+							<li>
+                <a href="#">
+                <Glyphicon glyph="star" /><p></p>
+								 Item 1</a>
+							</li>
+							<li>
+                <a href="#">
+                <Glyphicon glyph="star" /><p></p>
+								 Item 1</a>
+							</li>
 
-export default React.createClass( {
-    render: function() {
-        return <div id="sidebar-menu" className={styles.sideBarMenuContainer}>
-            <Navbar fluid className={styles.sidebar} inverse >
-
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="/">User Name</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-
-                <Navbar.Collapse>
-                    <Navbar.Text className={styles.userMenu}>
-                        <Navbar.Link href="#"><Glyphicon glyph="home"/></Navbar.Link>
-                        <Navbar.Link href="#"><Glyphicon glyph="log-out"/></Navbar.Link>
-                    </Navbar.Text>
-                    <Nav>
-                        <NavDropdown eventKey={1} title="Item 1">
-                            <MenuItem eventKey={1.1} href="#">Item 1.1</MenuItem>
-                        </NavDropdown>
-                        <NavItem eventKey={2}>Item 2</NavItem>
-                        <NavItem eventKey={3}>Item 3</NavItem>
-                    </Nav>
-                </Navbar.Collapse>
-
-            </Navbar>
-        </div>;
-    }
-});
+						</ul>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+export default SideBar;
